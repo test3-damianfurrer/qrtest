12a562ca8846f1c67c47599a505af1904dbc3bd6
210mlt <210mlt@gmail.com>
2022-09-10 06:19:52 +0200

    changed naming

--- write.c
+++ write.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+	#include <stdio.h>
 #include <stdlib.h>
 #define MAX_PATH 4096
 
 int main(int argc, char ** argv){
     FILE *fptr;
+    //char number[10]//not nulled;
+    char *number=(char*)calloc(10,sizeof(char));
     char *initinitfname="test.txt";
     char *initfname;
     char *fname;
@@ -24,8 +26,47 @@ int main(int argc, char ** argv){
 
     c=getc(stdin);
     int ind;
+
+    int fullnumber;
+
     while(c!=-1){
-        ind=filenumber%10;
+        fullnumber=filenumber;
+        int x=0;
+        while(fullnumber>0){
+            int numb=fullnumber%10;
+            switch (numb) {
+                case 0:
+                    number[x]='0'; break;
+                case 1:
+                    number[x]='1'; break;
+                case 2:
+                    number[x]='2'; break;
+                case 3:
+                    number[x]='3'; break;
+                case 4:
+                    number[x]='4'; break;
+                case 5:
+                    number[x]='5'; break;
+                case 6:
+                    number[x]='6'; break;
+                case 7:
+                    number[x]='7'; break;
+                case 8:
+                    number[x]='8'; break;
+                case 9:
+                    number[x]='9'; break;
+            }
+            fullnumber=fullnumber/10;
+            x++;
+        }
+//        fnamelen;
+        for(fnamelen=0;initfname[fnamelen];fnamelen++)
+          	fname[fnamelen]=initfname[fnamelen];
+        for(int i=9;i>=0;i--)
+            if(number[i]!='\0')
+                fname[fnamelen++]=number[i];
+        /*
+            ind=filenumber%10;
         switch (ind) {
         case 0:
             for(int i=0;initfname[i];i++)
@@ -62,6 +103,7 @@ int main(int argc, char ** argv){
             fnamelen++;
         break;
         }
+        */
         fptr=fopen(fname,"w");
         for(int i=0;i<4296;i++){
             putc(c,fptr);
